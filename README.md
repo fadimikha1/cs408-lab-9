585b5c1953d6ce2126ffdb89184b03386c1ee623
fadimikha1 <fadimikha@u.boisestate.edu>
2025-03-12 23:49:12 -0600

    submitting

--- README.md
+++ README.md
@@ -2,23 +2,12 @@
 
 ## Overview
 
-TODO: Write a project description
+This porject basically makes a cool animation where you start off with 25 balls that constantly decrease and change in color whenever they either hit an evil circle or the wall, respecitvley. 
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
+- https://dev.to/stewyearth/making-a-simple-counter-in-javascript-html-2ici
 
-A simple bulleted list below is sufficient. For example:
+- ChatGpt prompt for how to use document query selector in javascript and how to update a javascript counter for html text
 
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+-https://www.w3schools.com/jsref/jsref_constructor_class.asp
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const para = document.querySelector("p");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
@@ -18,14 +19,22 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -58,6 +67,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
+      if(ball.exists === true){
       if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
@@ -69,9 +79,79 @@ class Ball {
       }
     }
   }
+  }
+}
+
+class EvilCircle extends Shape {
+ constructor (x, y, velX, velY){
+  super(x, y, 20, 20);
+  this.color = 'white';
+  this.size = 10;
+  window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "a":
+        this.x -= this.velX;
+        break;
+      case "d":
+        this.x += this.velX;
+        break;
+      case "w":
+        this.y -= this.velY;
+        break;
+      case "s":
+        this.y += this.velY;
+        break;
+    }
+  });
+ }
+
+ draw() {
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+checkBounds() {
+  if (this.x + this.size >= width) {
+    this.x = width - this.size; //potentially ill need to update this to be + size? maybe due to alternate the addition and subtraction?
+  }
+
+  if (this.x - this.size <= 0) {
+    this.x = this.size; 
+  }
+
+  if (this.y + this.size >= height) {
+    this.x = width - this.size;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.y = this.size;
+  }
+}
+
+collisionDetect() {
+  for (const ball of balls) {
+    if(ball.exists === true){
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.exists = false;
+        ballCount--;
+        para.textContent = `Ball count: ${ballCount}`;
+      }
+    }
+  
+}
+}
+
 }
 
 const balls = [];
+ballCount = 0;
 
 while (balls.length < 25) {
   const size = random(10, 20);
@@ -85,19 +165,28 @@ while (balls.length < 25) {
     randomRGB(),
     size
   );
-
+  ballCount++;
+  para.textContent = `Ball count: ${ballCount}`;
   balls.push(ball);
 }
+const evilCircle = new EvilCircle(300, 500);
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
+
   for (const ball of balls) {
+    if (ball.exists) {
     ball.draw();
     ball.update();
     ball.collisionDetect();
+    }
   }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
